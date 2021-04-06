816c6d9b46a2d389c5df44e1b82a8366dc76a5da
Tomek Zymkowski <t.zymkowski@gmail.com>
2021-04-06 22:19:09 +0200

    Three

--- Blog.DAL.Tests/RepositoryTests.cs
+++ Blog.DAL.Tests/RepositoryTests.cs
@@ -138,7 +138,7 @@ namespace Blog.DAL.Tests
             repository.AddNewComment(new Comment() { Post = otherPost, Content = "Tomorrow never dies" });
             var commentsCountAfter = repository.GetPostComments(chosenPost).Count();
 
-            Assert.AreEqual(commentsCount + 2, commentsCountAfter);
+            Assert.AreEqual(commentsCount + 3, commentsCountAfter);
         }
 
     }
